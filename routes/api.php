c7b4c4ebad429e6bd40f6c554b0d9b31108f1fe6
Isi Ramirez <isiryder@gmail.com>
2021-08-29 21:04:55 +0200

    Add API routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\LibraryController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -26,3 +27,9 @@ Route::get('/authors/{id}', [AuthorController::class, 'show']);
 Route::post('/authors', [AuthorController::class, 'store']);
 Route::put('/authors/{id}', [AuthorController::class, 'update']);
 Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
+
+Route::get('/libraries', [LibraryController::class, 'index']);
+Route::get('/libraries/{id}', [LibraryController::class, 'show']);
+Route::post('/libraries', [LibraryController::class, 'store']);
+Route::put('/libraries/{id}', [LibraryController::class, 'update']);
+Route::delete('/libraries/{id}', [LibraryController::class, 'destroy']);
