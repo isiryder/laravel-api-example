247e05520c703476cf9d155baf36f23cfc841de9
Isi Ramirez <isiryder@gmail.com>
2021-08-30 22:32:11 +0200

    Modify seeder

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Book;
+use App\Models\Library;
 
 class BooksTableSeeder extends Seeder
 {
@@ -14,16 +16,26 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Book::truncate();
-
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $faker = \Faker\Factory::create();
 
+        $library = Library::create([
+            'name' => $faker->name,
+            'address' => $faker->address
+        ]);
+
         for ($i=0; $i < 50; $i++){
-            Book::create([
+            $book = Book::create([
                 'name' => $faker->name,
                 'year' => $faker->year,
-                'author_id' => 1
+                'author_id' => 1,
             ]);
+
+            $book->libraries()->save($library);
+
+            $book->save();
         }
     }
 }
