897c6c199cc324f5fab1ff7e78bf56657777da80
Isi Ramirez <isiryder@gmail.com>
2021-09-01 16:06:26 +0200

    Add tests

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -27,6 +27,65 @@ class BookTest extends TestCase
         $this->assertDatabaseHas('books', $payload);
     }
 
+   public function testCreateOneBookWithAuthorSuccesfully()
+   {
+        $payload = [
+            'name' => $this->faker->name,
+            'year' => $this->faker->year,
+            "author" => [
+                "name" => $this->faker->name,
+                "birth_date" => $this->faker->date,
+                "genre" => $this->faker->word
+            ],
+        ];
+
+        $response = $this->json('POST', 'api/books', $payload)
+            ->assertStatus(Response::HTTP_CREATED);
+
+        $this->json('GET', 'api/books/' . $response->json()['book']['id'])
+        ->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            "id",
+            "name",
+            "year",
+            "author_id",
+            "author" => [
+                "name",
+                "birth_date",
+                "genre"
+            ]
+        ]);
+    }
+
+    public function testCreateOneBookWithLibrarySuccesfully()
+    {
+         $payload = [
+             'name' => $this->faker->name,
+             'year' => $this->faker->year,
+             "library" => [
+                 "name" => $this->faker->name,
+                 "address" => $this->faker->date,
+             ],
+         ];
+ 
+         $response = $this->json('POST', 'api/books', $payload)
+             ->assertStatus(Response::HTTP_CREATED);
+ 
+         $this->json('GET', 'api/books/' . $response->json()['book']['id'])
+         ->assertStatus(Response::HTTP_OK)
+         ->assertJsonStructure([
+             "id",
+             "name",
+             "year",
+             "libraries" => [
+                0 => [
+                    "name",
+                    "address",
+                ]
+            ]
+         ]);
+     }
+
     public function testDeleteNonExistingBook()
     {
         $this->json('DELETE', 'api/books/0')
@@ -81,6 +140,92 @@ class BookTest extends TestCase
             ->assertStatus(Response::HTTP_NO_CONTENT);
 
         $this->assertDatabaseHas('books', $payload);
+    }
+
+    public function testUpdateOneBookWithAuthorSuccesfully()
+    {
+        $book = Book::create([
+            'name' => $this->faker->name,
+            'year' => $this->faker->year
+        ]);
+
+        $author = Author::create([
+            'name' => $this->faker->name,
+            'birth_date' => $this->faker->date,
+            'genre' => $this->faker->word
+        ]);
+
+        $book->author_id = $author->id;
+
+        $book->save();
+
+        $payload = $book->toArray();
+        $payload['author'] = $author->toArray();
+
+        $payload['name'] .= ' modified!';
+        $payload['author']['name'] .= ' modified!';
+
+        $this->json('PUT', 'api/books/' . $book->id, $payload)
+            ->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $response = $this->json('GET', 'api/books/' . $book->id)
+        ->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            "id",
+            "name",
+            "year",
+            "author_id",
+            "author" => [
+                "name",
+                "birth_date",
+                "genre"
+            ]
+        ]);    
+        $this->assertEquals($response->json()['author']['id'], $payload['author']['id']);
+        $this->assertEquals($response->json()['author']['name'], $payload['author']['name']);
+    }
+
+    public function testUpdateOneBookWithLibrarySuccesfully()
+    {
+        $book = Book::create([
+            'name' => $this->faker->name,
+            'year' => $this->faker->year
+        ]);
+
+        $library = Library::create([
+            'name' => $this->faker->name,
+            'address' => $this->faker->address
+        ]);
+
+        $book->libraries()->save($library);
+
+        $payload = $book->toArray();
+        $payload['libraries'] = [
+            0 => $library->toArray()
+        ];
+
+        $payload['name'] .= ' modified!';
+        $payload['libraries'][0]['name'] .= ' modified!';
+
+        $this->json('PUT', 'api/books/' . $book->id, $payload)
+            ->assertStatus(Response::HTTP_NO_CONTENT);
+
+        $response = $this->json('GET', 'api/books/' . $book->id)
+        ->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            "id",
+            "name",
+            "year",
+            "author_id",
+            "libraries" => [
+                0 => [
+                    "name",
+                    "address",
+                ]
+            ]
+        ]);    
+        $this->assertEquals($response->json()['libraries'][0]['id'], $payload['libraries'][0]['id']);
+        $this->assertEquals($response->json()['libraries'][0]['name'], $payload['libraries'][0]['name']);
     }
 
     public function testUpdateNonExistingBook()
