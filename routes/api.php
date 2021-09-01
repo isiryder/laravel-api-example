5475c4c21a047c6ca5cce930759ff3991877f6f4
Isi Ramirez <isiryder@gmail.com>
2021-09-01 16:05:59 +0200

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\LibraryController;
 
 Route::get('/books', [BookController::class, 'index']);
 Route::get('/books/{id}', [BookController::class, 'show']);
-Route::post('/books', [BookController::class, 'store']);
-Route::put('/books/{id}', [BookController::class, 'update']);
-Route::delete('/books/{id}', [BookController::class, 'destroy']);
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');;
+Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
 
 Route::get('/authors', [AuthorController::class, 'index']);
 Route::get('/authors/{id}', [AuthorController::class, 'show']);
